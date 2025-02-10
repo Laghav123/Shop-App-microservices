a3ecf1da45e36bc68b8e066a83e2503116d167b6
Laghav Mohan <lmohan@zlti.com>
2025-02-11 00:42:39 +0530

    Add swagger endpoints to the API Gateway service.

--- api-gateway/src/main/java/com/triton/api_gateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/triton/api_gateway/config/SecurityConfig.java
@@ -9,12 +9,22 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
+    private final String[] freeResourceURLs = {
+            "/swagger-ui.html",
+            "/swagger-ui/**",
+            "/api-docs/**",
+            "/swagger-resources/**",
+            "/aggregate/**"
+    };
+
     // SecurityFilterChain becomes kind of middleware for security
     // it intercepts all the request and check if it requires authorisation
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity.authorizeHttpRequests(
-                authorize -> authorize.anyRequest().authenticated()     // it means that any request coming needs to be authorised
+                authorize -> authorize
+                        .requestMatchers(freeResourceURLs).permitAll()
+                        .anyRequest().authenticated()     // it means that any request coming needs to be authorised
         ).oauth2ResourceServer(
                 oauth2 -> oauth2.jwt(Customizer.withDefaults())     // it means that request will be validated on basis of jwt tokens
         ).build();
--- api-gateway/src/main/java/com/triton/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/triton/api_gateway/routes/Routes.java
@@ -8,6 +8,8 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
+
 @Configuration
 public class Routes {
 
@@ -29,6 +31,30 @@ public class Routes {
     public RouterFunction<ServerResponse> inventoryRouteMatcher() {
         return GatewayRouterFunctions.route("inventory_service")
                 .route(RequestPredicates.path("api/inventory"), HandlerFunctions.http("http://localhost:8083"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> productSwaggerRouteMatcher() {
+        return GatewayRouterFunctions.route("product_service_swagger")
+                .route(RequestPredicates.path("/aggregate/product-service/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderSwaggerRouteMatcher() {
+        return GatewayRouterFunctions.route("order_service_swagger")
+                .route(RequestPredicates.path("/aggregate/order-service/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventorySwaggerRouteMatcher() {
+        return GatewayRouterFunctions.route("inventory_service_swagger")
+                .route(RequestPredicates.path("/aggregate/inventory-service/api-docs"), HandlerFunctions.http("http://localhost:8083"))
+                .filter(setPath("/api-docs"))
                 .build();
     }
 
