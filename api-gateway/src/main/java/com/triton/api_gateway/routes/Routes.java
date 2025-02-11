69eadc453e513480cfece180a0b9544a72ed436a
Laghav Mohan <lmohan@zlti.com>
2025-02-11 21:09:48 +0530

    Integrated Circuit Breakers for all routes in API Gateway and for Rest client InventoryClient in Order Service

--- api-gateway/src/main/java/com/triton/api_gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/triton/api_gateway/routes/Routes.java
@@ -1,12 +1,16 @@
 package com.triton.api_gateway.routes;
 
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
+
+import java.net.URI;
 
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 
@@ -17,6 +21,12 @@ public class Routes {
     public RouterFunction<ServerResponse> productRouteMatcher() {
         return GatewayRouterFunctions.route("product_service")
                 .route(RequestPredicates.path("api/product"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(
+                        CircuitBreakerFilterFunctions.circuitBreaker(
+                                "productServiceCircuitBreaker",
+                                URI.create("forward:/fallbackRoute")
+                        )
+                )
                 .build();
     }
 
@@ -24,6 +34,12 @@ public class Routes {
     public RouterFunction<ServerResponse> orderRouteMatcher() {
         return GatewayRouterFunctions.route("order_service")
                 .route(RequestPredicates.path("api/order"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(
+                        CircuitBreakerFilterFunctions.circuitBreaker(
+                                "orderServiceCircuitBreaker",
+                                URI.create("forward:/fallbackRoute")
+                        )
+                )
                 .build();
     }
 
@@ -31,6 +47,12 @@ public class Routes {
     public RouterFunction<ServerResponse> inventoryRouteMatcher() {
         return GatewayRouterFunctions.route("inventory_service")
                 .route(RequestPredicates.path("api/inventory"), HandlerFunctions.http("http://localhost:8083"))
+                .filter(
+                        CircuitBreakerFilterFunctions.circuitBreaker(
+                                "inventoryServiceCircuitBreaker",
+                                URI.create("forward:/fallbackRoute")
+                        )
+                )
                 .build();
     }
 
@@ -38,6 +60,12 @@ public class Routes {
     public RouterFunction<ServerResponse> productSwaggerRouteMatcher() {
         return GatewayRouterFunctions.route("product_service_swagger")
                 .route(RequestPredicates.path("/aggregate/product-service/api-docs"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(
+                        CircuitBreakerFilterFunctions.circuitBreaker(
+                                "productSwaggerServiceCircuitBreaker",
+                                URI.create("forward:/fallbackRoute")
+                        )
+                )
                 .filter(setPath("/api-docs"))
                 .build();
     }
@@ -46,6 +74,12 @@ public class Routes {
     public RouterFunction<ServerResponse> orderSwaggerRouteMatcher() {
         return GatewayRouterFunctions.route("order_service_swagger")
                 .route(RequestPredicates.path("/aggregate/order-service/api-docs"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(
+                        CircuitBreakerFilterFunctions.circuitBreaker(
+                                "orderSwaggerServiceCircuitBreaker",
+                                URI.create("forward:/fallbackRoute")
+                        )
+                )
                 .filter(setPath("/api-docs"))
                 .build();
     }
@@ -54,8 +88,24 @@ public class Routes {
     public RouterFunction<ServerResponse> inventorySwaggerRouteMatcher() {
         return GatewayRouterFunctions.route("inventory_service_swagger")
                 .route(RequestPredicates.path("/aggregate/inventory-service/api-docs"), HandlerFunctions.http("http://localhost:8083"))
+                .filter(
+                        CircuitBreakerFilterFunctions.circuitBreaker(
+                                "inventorySwaggerServiceCircuitBreaker",
+                                URI.create("forward:/fallbackRoute")
+                        )
+                )
                 .filter(setPath("/api-docs"))
                 .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallbackRouteMatcher() {
+        return GatewayRouterFunctions.route("fallbackRoute")
+                .GET(
+                        "/fallbackRoute",
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE)
+                            .body("Service is unavailable, please try again later")
+                ).build();
     }
 
 }
--- order-service/src/main/java/com/triton/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/triton/order/config/RestClientConfig.java
@@ -2,12 +2,17 @@ package com.triton.order.config;
 
 import com.triton.order.client.InventoryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
 
 @Configuration
 public class RestClientConfig {
@@ -19,10 +24,17 @@ public class RestClientConfig {
 
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryURL)
+                .requestFactory(getClientRequestFactory())
                 .build();
         RestClientAdapter restClientAdapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         InventoryClient inventoryClient = httpServiceProxyFactory.createClient(InventoryClient.class);
         return inventoryClient;
+    }
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout((Duration.ofSeconds(3)));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
     }
 }
